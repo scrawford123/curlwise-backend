c944c04790a0420ae21feef2660378aafdb579bf
scrawford123 <sahana.crawford@icloud.com>
2025-07-06 21:18:22 -0400

    commit

--- app.py
+++ app.py
@@ -89,3 +89,7 @@ def analyze():
 @app.route("/")
 def home():
     return "Curlwise backend is live!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
