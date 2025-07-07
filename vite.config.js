83407272fc490e2492a6439285ec433e844d07ff
Sahana Rose <sahanarose@MacBook-Air-4.local>
2025-07-07 00:50:59 -0400

    Initial React frontend commit

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc"; // Use SWC plugin for React
 
 // https://vitejs.dev/config/
-export default defi
+export default defineConfig({
+  plugins: [react()],
+});
