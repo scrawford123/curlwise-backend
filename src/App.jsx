54a9b1799990f2f95d65b244235ea044b27c9a4f
Sahana Rose <sahanarose@MacBook-Air-4.local>
2025-07-07 00:06:24 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import CurlwiseUploader from "./CurlwiseUploader";
+
+export default function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-50">
+      <CurlwiseUploader />
+    </main>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc"; // Use SWC plugin for React
+
+// https://vitejs.dev/config/
+export default defi
